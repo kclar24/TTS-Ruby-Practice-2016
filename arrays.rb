6df5990dd3c5d07d534eac89cc4f52e6a11bd178
Keith A. Clark <geauxlsutigers24@gmail.com>
2016-10-19 10:31:39 -0500

    practice with arrays

---
+++ arrays.rb
@@ -0,0 +1,73 @@
+# scores = [100, 85, 30, 79]
+# counter = 0
+# sum = 0
+#
+# # while counter < scores.length
+# #   sum = sum + scores[counter]
+# #   counter += 1 # same as counter = counter + 1
+# # end
+# scores.each do |index|
+#   sum = sum + index
+# end
+# puts "The total is #{sum}"
+# ___________________________________________________
+
+# scores = [100, 85, 30, 79]
+# counter = 0
+# sum = 0
+#
+# scores.each do |index|
+#   sum = sum + index
+# end
+# avg = sum / scores.length
+# puts "The average is #{avg}"
+# ____________________________________________________
+
+# scores = [100, 85, 30, 79]
+# puts scores
+#
+# puts "Input the first score"
+# new_score = gets.to_i
+# scores << new_score
+# puts scores
+#
+# puts "Input the second score"
+# new_score = gets.to_i
+# scores.push(new_score)
+# puts scores
+# ____________________________________________________
+
+# scores = [100, 85, 30, 79]
+# puts scores
+#
+# scores2 = [200, 170, 60, 158]
+#
+# scores2.each do |score|
+#   scores << score
+# end
+#
+# puts "the array with newly added scores is:"
+# puts scores
+# _______________________________________________________
+
+# scores = ["Let's shift!", 100, 85, 30, 79]
+# puts scores
+#
+# puts "We are shifting the array"
+# puts scores.shift
+#
+# puts "the array is now at:"
+# puts scores
+#
+# puts "now we will unshift"
+# test_string = "unshift!"
+# scores.unshift(test_string)
+#
+# puts "the array after an unshift is:"
+# puts scores
+# ________________________________________________________
+
+# #Nested Arrays
+# array1 = [ [ 1, 2, 3 ], [ 4, 5, 6, ] ]
+# puts array1[0][2]
+# _________________________________________________________
