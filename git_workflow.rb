05988292d5f581ff13504438c2c7538e3e46133a
Keith A. Clark <geauxlsutigers24@gmail.com>
2016-10-20 08:36:33 -0500

    adding git workflow file

---
+++ git_workflow.rb
@@ -0,0 +1,26 @@
+git remote add remote_name URL
+git checkout -b new_branch
+git branch
+
+DO SOME WORK
+
+git diff
+git status
+
+git add .
+git commit -m "message of work completed"
+
+git push origin new_branch
+
+git checkout master
+git branch
+
+git merge new_branch m "merging branch new_branch into branch master"
+
+git push origin master
+
+git push staging master
+TESTING ON staging
+
+git push production master
+Make sure production is NOT broken
